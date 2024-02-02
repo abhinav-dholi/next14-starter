d57e75814cdbb6e93635ec94e21783be0fa31fab
abhinav-dholi <81688737+abhinav-dholi@users.noreply.github.com>
2024-02-02 16:13:03 -0500

    homepage design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Links from './links/Links'
 import styles from  "./navbar.module.css"
 const Navbar = () => {
@@ -5,9 +6,7 @@ const Navbar = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>
-        Logo
-      </div>
+      <Link href="/" className={styles.logo}>Logo</Link>
       <div>
       <Links/>
       </div>
