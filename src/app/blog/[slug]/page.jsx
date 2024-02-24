8aa1d52d8954f5ae572dbee8ceb01d239623cbb4
abhinav-dholi <81688737+abhinav-dholi@users.noreply.github.com>
2024-02-24 11:16:24 -0500

    backend setup with mongodb atlas
    SEO and password hashing left

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -26,24 +26,18 @@ const SinglePostPage = async ({ params }) => {
   console.log(post);
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
+     {post.img && <div className={styles.imgContainer}>
         <Image
           className={styles.img}
-          src="https://images.pexels.com/photos/19936793/pexels-photo-19936793/free-photo-of-vicmary.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src={post.img}
           alt=""
           fill
         ></Image>
-      </div>
+      </div>}
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post?.title}</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image
-            className={styles.avatar}
-            src="https://images.pexels.com/photos/19936793/pexels-photo-19936793/free-photo-of-vicmary.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            width={50}
-            height={50}
-          ></Image>
+          
           {post && (
             <Suspense fallback={<div>Loading...</div>}>
               <PostUser userId={post.userId} />
@@ -51,10 +45,10 @@ const SinglePostPage = async ({ params }) => {
           )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>01.01.2024</span>
+            <span className={styles.detailValue}>{post.createdAt.toString().slice(4,16)}</span>
           </div>
         </div>
-        <div className={styles.content}>{post?.body}</div>
+        <div className={styles.content}>{post.desc}</div>
       </div>
     </div>
   );
--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,5 +1,6 @@
 import { getUser } from "@/lib/data";
 import styles from "./postUser.module.css";
+import Image from "next/image";
 
 // const getData  = async (userId)=>{
 //     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {cache: "no-store"})
@@ -15,8 +16,17 @@ const PostUser = async ({ userId }) => {
   const user = await getUser(userId);
   return (
     <div className={styles.container}>
+      <Image
+            className={styles.avatar}
+            src={user.img ? user.img: "/noavatar.png"}
+            alt=""
+            width={50}
+            height={50}
+          ></Image>
+      <div className={styles.texts}>
       <span className={styles.title}>Author</span>
       <span className={styles.username}>{user.username}</span>
+      </div>
     </div>
   );
 };
